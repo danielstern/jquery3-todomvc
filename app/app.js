301a932326a90d2d51df0b8468200e0a9410ac2c
Code Whisperer <daniel@danielstern.ca>
2016-04-26 13:20:04 -0400

    added list with checkboxes

--- app/app.js
+++ app/app.js
@@ -1,13 +1,23 @@
 // Here a deprecated call of .load is used which will break when updating jQuery
 $(window).load(()=>{
 	console.log(`Application loaded, running ${$.fn.jquery}`);
-});
 
-// The usage of complete is not supported in ^3.0.0
-$.ajax('/items')
-.complete((d)=>{
-	console.log("Loaded data",d);
-})
+
+	$.getJSON('/items')
+	.complete((data)=>{
+		console.log("Loaded data",data);
+		 data.responseJSON.forEach((d)=>{
+			 console.log(d);
+	 		$("#target").append(`
+	 			<li>
+	 				${d.title}
+					<input type="checkbox" ${d.complete ? "checked" : ""}/>
+	 			</li>
+	 		`)
+		});
+	});
+
+});
 
 // this causes an error with new jQuery
 $("#");
