08c341fc287428954c4a830ed0defb0502db1e16
Code Whisperer <daniel@danielstern.ca>
2016-04-26 13:50:29 -0400

    Added showing and hiding with jQuery 2

--- app/app.js
+++ app/app.js
@@ -1,23 +1,55 @@
 // Here a deprecated call of .load is used which will break when updating jQuery
+let items = [];
+let showComplete = true;
 $(window).load(()=>{
 	console.log(`Application loaded, running ${$.fn.jquery}`);
-
+	$("#showCompleted").change((e)=>{
+		console.log("clicked",e.target);
+		showComplete = e.target.checked;
+		render(items,showComplete);
+	});
 
 	$.getJSON('/items')
 	.complete((data)=>{
-		console.log("Loaded data",data);
-		 data.responseJSON.forEach((d)=>{
-			 console.log(d);
-	 		$("#target").append(`
-	 			<li>
-	 				${d.title}
-					<input type="checkbox" ${d.complete ? "checked" : ""}/>
-	 			</li>
-	 		`)
-		});
+		//console.log("Loaded data",data);
+		items = data.responseJSON;
+		render();
 	});
 
 });
+
+let render = ()=>{
+	let target = $("#target");
+	target.empty();
+	//debugger;
+	items
+		.filter((d)=>showComplete||!d.complete)
+		.forEach((d)=>{
+			$("#target").append(`
+				<li data=${d.id}>
+					${d.title}
+					<input type="checkbox" ${d.complete ? "checked" : ""}/>
+				</li>
+			`)
+		});
+
+	target
+		.find('input')
+		.change((e)=>{
+			console.log("Changed an item",e.target);
+			id = $(e.target.parentElement).attr('data');
+			items = items.map((i)=>{
+				if (i.id.toString() == id) {
+					i.complete = e.target.checked;
+				}
+				return i;
+			});
+			render();
+			//console.log(items);
+			//debugger;
+			//console.log(e);
+		})
+}
 
 // this causes an error with new jQuery
 $("#");
