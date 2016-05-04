e274feddc1005065d9229ede548c80368526637e
Code Whisperer <daniel@danielstern.ca>
2016-05-04 08:52:22 -0400

    some corrections to course files

--- app/app.js
+++ app/app.js
@@ -73,7 +73,7 @@ let render = ()=>{
 		.forEach((d)=>{
 			$("#target").append(
 				`
-					<li data-item-id=${d.id}>
+					<li data-item-id-1=${d.id}>
 					${d.title}
 					<input type="checkbox" ${d.complete ? "checked" : ""}/>
 				</li>
